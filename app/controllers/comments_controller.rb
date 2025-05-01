5e906f10fa648e8037bb5a32a8fc7398547e2c88
Sabreena Salomon <salomonbree@gmail.com>
2025-05-01 03:31:54 +0000

    debugged details.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.new
     comment.author_id = params.fetch("query_author_id")
-    comment.venue_id = params.fetch("query_venue_id")
+    comment.venue_id = params.fetch("venue_id")
     comment.body = params.fetch("query_body")
     comment.save
     
